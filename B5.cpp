a81cfdf349c288281a2215ecd207fa3b9f8c4660
Dinhlu24 <dinhlunhut@gmail.com>
2024-12-27 13:33:22 +0700

    Update B5.cpp

--- B5.cpp
+++ B5.cpp
@@ -1,5 +1,16 @@
 #include <stdio.h>
 
+int isPrime(int n);
+void printPrime(int x);
+
+int main(){
+	int a,b;
+	printf("Moi nhap vao so nguyen A: ");	scanf("%d", &a);
+	printf("Moi nhap vao so nguyen B: ");	scanf("%d", &b);
+	printPrime(a);
+	printPrime(b);
+}
+
 int isPrime(int n){
 	if(n < 2)	return 0;
 	for(int i=2;i*i<=n;i++){
@@ -8,10 +19,7 @@ int isPrime(int n){
 	return 1;
 }
 
-int main(){
-	for(int i=1;i<=2;i++){
-		int n=0;	printf("Moi nhap vao so nguyen thu %d: ",i);	scanf("%d", &n);
-		if(isPrime(n))	printf("%d la mot so nguyen to !!!\n",n);
-		else	printf("%d khong phai la mot so nguyen to !!!\n",n);
-	}
+void printPrime(int x){
+	if(isPrime(x))	printf("True\n");
+	else	printf("False\n");
 }
